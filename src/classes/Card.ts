04de02a00b603877b6311f196f47abd9a09c9ba0
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-11-23 07:54:14 -0300

    commit init

---
+++ src/classes/Card.ts
@@ -0,0 +1,8 @@
+export default class Card {
+    private id: string;
+    private titulo: string
+    private descricao: string
+    private data: string
+    private status: "pendente" | "concluido"
+    private prioridade: string
+}
